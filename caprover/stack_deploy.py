5bab940c69743a299f1bf3653fccf94b34f5ef80
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2025-08-05 14:37:00 -0500

    script REQUIRES --config-file arg

--- caprover/stack_deploy.py
+++ caprover/stack_deploy.py
@@ -324,6 +324,7 @@ def main():
     parser.add_argument(
         "-c",
         "--config-file",
+        required=True,
         help="Path to configuration YAML file (copy stack.example.yaml)",
     )
     parser.add_argument(
